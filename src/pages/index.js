3055c39866bf77df78eb1b0a83359072059d08f7
曾小皮-ZengXiaoPi <102937102+ZengXiaoPi@users.noreply.github.com>
2023-05-02 13:34:59 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/utgame">
             下载游戏
           </Link>
         </div>
